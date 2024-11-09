f1f97ccd141b933517c5bc7a85e15a9b761cbb27
Vishal Chhadekar <vishal.chhadekar@aionos.ai>
2024-11-09 17:04:25 +0530

    env file changes

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,14 +1,13 @@
 // services/auth.service.ts
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-// import { environment } from '../../environments/environment';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  // private apiUrl = `${environment.apiUrl}/auth`;
-  private apiUrl = ``;
+  private apiUrl = `${environment.apiUrl}/auth`;
 
   constructor(private http: HttpClient) {}
 
