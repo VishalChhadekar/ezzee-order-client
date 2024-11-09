eb08097d3db735384a9ebe551810a5f1919fb925
Vishal Chhadekar <vishal.chhadekar@aionos.ai>
2024-11-09 15:02:39 +0530

    updated env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,4 @@
-// environment.ts
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:3000/api'  // Replace with your deployed backend URL in production
+    apiUrl: 'http://localhost:3000/api'  
 };
