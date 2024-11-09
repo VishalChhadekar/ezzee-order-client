8e8bbb8d9d005dfa5e469c7e0cecaf921a8b910b
Vishal Chhadekar <vishal.chhadekar@aionos.ai>
2024-11-09 13:06:22 +0530

    Designed shoppig cart model

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { SharedModule } from '../../shared.module';
 import { ActivatedRoute, Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { ProductService, Product } from '../../services/product.service';
+
 import { FormsModule } from '@angular/forms';
+import { CartServiceService } from '../../services/cart-service.service';
 
 @Component({
   selector: 'app-header',
@@ -21,17 +23,39 @@ export class HeaderComponent implements OnInit {
   searchQuery: string = '';
   products: Product[] = [];
   filteredProducts: Product[] = [];
+  isCartModalOpen: boolean = false;
+  cartItems: Product[] = [];
 
   constructor(
     private productService: ProductService,
     private route: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private cartService: CartServiceService
   ) { }
 
   ngOnInit(): void {
+    this.getCartItems();
   }
 
   onSearch(): void {
+    // Implement search functionality
+  }
 
+  toggleCartModal(): void {
+    this.isCartModalOpen = !this.isCartModalOpen;
+    if (this.isCartModalOpen) {
+      this.getCartItems();
+    }
+  }
+
+  getCartItems(): void {
+    this.cartItems = this.cartService.getCartItems();
+  }
+  // Remove from cart function
+  removeFromCart(index: number): void {
+    this.cartItems.splice(index, 1);
+  }
+  getTotalPrice(): number {
+    return this.cartItems.reduce((total, item) => total + item.price, 0);
   }
 }
